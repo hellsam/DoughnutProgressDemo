8133c9e81a02cc18f7445754b440e690589df14f
hellsam <hellsam@yeah.net>
2015-12-27 01:56:11 +0800

    first commit

---
+++ doughnutprogress/src/main/java/com/hellsam/doughnutprogress/DoughnutProgress.java
@@ -0,0 +1,22 @@
+package com.hellsam.doughnutprogress;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by binshenchen on 15/12/27.
+ */
+public class DoughnutProgress extends View {
+    public DoughnutProgress(Context context) {
+        super(context);
+    }
+
+    public DoughnutProgress(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DoughnutProgress(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
